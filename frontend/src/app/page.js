658f693dc9bc9915cac27b4bebd7e191f3c07f50
edwingd18 <edwin.guerrero@uao.edu.co>
2025-11-05 00:28:10 -0500

    Prueba

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -104,7 +104,7 @@ export default function Dashboard() {
             textAlign="center"
             fontWeight="bold"
           >
-            ✅ CI/CD Activo con Jenkins + Railway
+            ✅ CI/CD Activo con Jenkins + Railway CAMBIO!!
           </Text>
           <Text fontSize="md" color="whiteAlpha.900" textAlign="center" mt={2}>
             Deploy automático desde rama develop
