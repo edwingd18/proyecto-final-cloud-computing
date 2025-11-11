cefca2b9e70f66dfeb1b3327a17823fe6e7e0075
edwingd18 <edwin.guerrero@uao.edu.co>
2025-11-11 17:04:36 -0500

    Update page.js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -96,7 +96,7 @@ export default function Dashboard() {
           boxShadow="2xl"
         >
           <Heading size="2xl" color="white" mb={3} textAlign="center">
-            🚀 Sistema de Gestión de Activoss
+            Prueba de deploy automatico
           </Heading>
           <Text
             fontSize="xl"
